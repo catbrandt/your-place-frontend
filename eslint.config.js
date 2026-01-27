2563ada590c625b0adf825ccef3fea667e45cb10
catbrandt <16284@coderacademy.edu.au>
2026-01-27 13:17:30 +1100

    prettier and eslint config 2

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,14 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import prettier from 'eslint-config-prettier'
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default [
+  { ignores: ['dist'] },
   {
     files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -22,8 +18,43 @@ export default defineConfig([
         sourceType: 'module',
       },
     },
+    settings: { 
+      react: { version: '19.2' } 
+    },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      // ESLint recommended
+      ...js.configs.recommended.rules,
+      
+      // React recommended
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      
+      // React Hooks
+      ...reactHooks.configs.recommended.rules,
+      
+      // Prettier (disables conflicting formatting rules)
+      ...prettier.rules,
+      
+      // Custom rules
+      'no-unused-vars': ['warn', { 
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
+      }],
+      'no-console': 'warn',
+      'prefer-const': 'error',
+      
+      // React specific
+      'react/prop-types': 'warn',
+      'react/jsx-props-no-spreading': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
     },
   },
-])
+]
